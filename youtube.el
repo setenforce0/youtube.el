33af3b6ae041ebed25dab7fe5bd30d7768d54d27
Kadin Buckton <kadin@linux-scs3.hitronhub.home>
2017-07-05 03:32:42 -0400

    New yt-search-videos function

--- youtube.el
+++ youtube.el
@@ -1,17 +1,23 @@
 (require 'request)
-(setq cid "457835332683-7k3hgag48hd5rpseahsik6s76cdd71kf.apps.googleusercontent.com"
-      csec "dgRS0Qm9Z0Mi24QM1bk6rii4")
-
+;; (setq cid "457835332683-7k3hgag48hd5rpseahsik6s76cdd71kf.apps.googleusercontent.com"
+;;       csec "dgRS0Qm9Z0Mi24QM1bk6rii4")
 ;; (setq user-input (read-string "String: "))
+(setq authtoken "ya29.Glt-BMPipsaTquQ96yM4_JqVd-CLfmY-a5bOq6IyDH2gpksCTGVfyf3NOb92eHRB2xOTQliaC-SpYXXHY1FrSK262DekTUo3DuuHaNP9n47V-N8wju5jl4jqBkUb")
 
-(request
- "https://www.googleapis.com/youtube/v3/search"
- :params '(("part" . "id,snippet") ("q" . "haskell"))
- :headers '(("Authorization" . "Bearer ya29.Glx9BPB2bovLoCqs0E3AHiHt5kscfu1vOTrW4zVLcUZ3p4ANFZV7_ZpzH5UrmpU0qMoeOcHXY4jknKvRdYiKqIhtmaE494uKzKxWPq81DbsFn4jgoT1eiBoiFNoDJw"))
- :parser 'json-read
- :success (function*
-           (lambda (&key data &allow-other-keys)
-             (let* ((item (elt (assoc-default 'items data) 0))
-                    (snippet (assoc-default 'snippet item))
-                    (title (assoc-default 'title snippet)))
-               (message "%s" title)))))
+(defun yt-search-videos (query authtoken)
+  (request
+   "https://www.googleapis.com/youtube/v3/search"
+   :params `(("part" . "id,snippet") ("q" . ,query))
+   :headers `(("Authorization" . ,(concat "Bearer " authtoken)))
+   :parser 'json-read
+   :success (function*
+             (lambda (&key data &allow-other-keys)
+               (let* ((item (elt (assoc-default 'items data) 0 ))
+                      (snippet (assoc-default 'snippet item))
+                      (title (assoc-default 'title snippet)))
+               (let ((buffer (get-buffer-create "*youtube*")))
+                 (with-current-buffer buffer
+                   (insert title))
+                 (switch-to-buffer buffer)))))))
+
+(yt-search-videos "haskell" authtoken)
